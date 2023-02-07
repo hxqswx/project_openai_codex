a610299ecc8dc9a7049b1a0da4620652751b169a
mikexiangwang <mikexiangwang@gmail.com>
2023-02-07 19:34:40 +0000

    update for quanquan

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-import bot from './assets/bot.svg'
-import user from './assets/user.svg'
+import bot from './assets/bot.webp'
+import user from './assets/user.webp'
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
